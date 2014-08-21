68dcd37f11b226160ed1bca43303f7fd4d122750
cfphpflex <cfphpflex@gmail.com>
2014-08-20 17:44:21 -0700

    test
    test

--- ModelSampleGetData.php
+++ ModelSampleGetData.php
@@ -5,53 +5,47 @@
  */
 	
 
-	 		// ENV
- 			$this ->environment 	= isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:'localhost';   //Page user is on
+	 	// ENV
+ 		$this ->environment 	= isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:'localhost';   //Page user is on
 			
 		 
-	 		// DECLARE 	MYSQL HOST INFO
-		    $this ->hostIP = "127.0.0.1";	// API
-			$this ->id = 0;            		// id
-			$this ->UserName= "root";  		// admin usr for db
-		 	//DECLARE Default Params
+	 	// DECLARE 	MYSQL HOST INFO
+	    	$this ->hostIP = "127.0.0.1";	// API
+		$this ->id = 0;            		// id
+		$this ->UserName= "that";  		// admin usr for db
+	 	//DECLARE Default Params
 		 	
-		 	 
-		 		/*	foreach ( $_POST as $var => $val)
-		 			{
-		 				$this -> $var 			= isset($_POST['$var'])?$_POST['$val']:"";   	 //Page user is on
-		 			}
-		 	   */
-		 	
-		 	// DECLARE SEARCH PARAM DEFAULTS
-		 	$this -> page 			= isset($_POST['page'])?$_POST['page']:1;   	 //Page user is on
-		 	$this -> rows 			= isset($_POST['rows'])?$_POST['rows']:10;  	//Number of Rows to display per page
-		 	$this -> sidx 			= isset($_POST['sidx'])?$_POST['sidx']:"";  	//Sort Column
-		 	$this -> sord 			= isset($_POST['sord'])?$_POST['sord']:"ASC";  //Sort Order
-		 	$this -> nd 			= isset($_POST['nd'])?$_POST['nd']:0;    
-		 	$this -> _search 		= isset($_POST['_search'])?$_POST['_search']:false;  		//Whether search is performed by user on data grid
-		 	$this -> searchField 	= isset($_POST['searchField'])?$_POST['searchField']:"";  	//Field to perform Search on
-		 	$this -> searchOper 	= isset($_POST['searchOper'])?$_POST['searchOper']:"";  	//Search Operator Short Form"
-		 	$this -> searchString 	= isset($_POST['searchString'])?$_POST['searchString']:"";   //Search Text
+		 
+		// DECLARE SEARCH PARAM DEFAULTS
+	 	$this -> page 		= isset($_POST['page'])?$_POST['page']:1;   	 //Page user is on
+	 	$this -> rows 		= isset($_POST['rows'])?$_POST['rows']:10;  	//Number of Rows to display per page
+	 	$this -> sidx 		= isset($_POST['sidx'])?$_POST['sidx']:"";  	//Sort Column
+	 	$this -> sord 		= isset($_POST['sord'])?$_POST['sord']:"ASC";  //Sort Order
+	 	$this -> nd 		= isset($_POST['nd'])?$_POST['nd']:0;    
+	 	$this -> _search 	= isset($_POST['_search'])?$_POST['_search']:false;  		//Whether search is performed by user on data grid
+	 	$this -> searchField 	= isset($_POST['searchField'])?$_POST['searchField']:"";  	//Field to perform Search on
+	 	$this -> searchOper 	= isset($_POST['searchOper'])?$_POST['searchOper']:"";  	//Search Operator Short Form"
+	 	$this -> searchString 	= isset($_POST['searchString'])?$_POST['searchString']:"";   //Search Text
 		  
-		  	$this -> arrUsers 	= array(); 
-		    $this -> strMsg 	= "";
-		    $this -> strMsgType 	= "success";
-		    $this -> records 	= "";
-		    $this -> blnSearch 	= "";
-		    $this -> strMsg 	= false;
-		    $this -> strSearch 	= "";
+	  	$this -> arrUsers 	= array(); 
+	    	$this -> strMsg 	= "";
+	    	$this -> strMsgType 	= "success";
+	    	$this -> records 	= "";
+		$this -> blnSearch 	= "";
+		$this -> strMsg 	= false;
+		$this -> strSearch 	= "";
 		    
 		    
 		    
 		    
 			 try{
 	               //Production
-	               //$this->_db = new PDO("mysql:dbname=hrportal;host=$hostIP", "root", "$4@2@m!HW");
+
 	               
 	              // Development 
 	              
 	 			// 1.  DB connection
-				 $this->_db = new PDO("mysql:dbname=hrportal;host=$this->hostIP", "root", "");
+				 $this->_db = new PDO("mysql:dbname=hrportal;host=$this->hostIP", "that", "");
 	              //$this->_db = new PDO("mysql:dbname=hrportal;host=$hostIP", "root", "");
         			//Debug
         			//echo("pdo connectiond done". "<br>");
