a8d32c3107342a59b02213274cc0de108c9d1baf
cfphpflex <cfphpflex@gmail.com>
2014-10-30 12:30:15 -0700

    simple helper
    simple helper

--- HelperSampleClass.php
+++ HelperSampleClass.php
@@ -1,8 +1,9 @@
 <?php
-/**
+/** example showing how to make a helper function
  * Make a helper function or use built in get_hostname  function to get the environtments hostname
+ * first test if the function not exist, then proceed to load function
  * this addresses the the basic need to know what environment code is running for configuration purposes
-  
+ * gets the hostname from the server scope and returns the hostname in teh $field variable
  */
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
@@ -17,8 +18,8 @@ if (!function_exists('my_host'))
 	 { 
 	 	 //	public  $protocol = strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https')=== FALSE ? 'http' : 'https';
 	 		  
-	 		 $params   = $_SERVER['QUERY_STRING'];
-                  	$field['url'] 		=  	$field['protocol']  . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];  
+	 		$params   = $_SERVER['QUERY_STRING'];
+                       	$field['url'] 		=  	$field['protocol']  . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];  
 			 
 			  	
 			return $field; 		 
